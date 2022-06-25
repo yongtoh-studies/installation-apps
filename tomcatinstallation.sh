c8357b8cb894dc19e4e1c0831819404797f3c15b
yongtoh <yongtoh@gmail.com>
2022-06-25 22:23:48 +0000

    first version

---
+++ tomcatinstallation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Use this script to install tomcat in rehat servers
+echo delete the failed version of tomcat
+sudo rm -rf /opt/tomcat9
+echo assign a hostname to your server 
+sudo hostname tomcat2
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+cd /opt 
+sudo yum install git wget -y
+sudo yum install java-1.8.0-openjdk-devel -y
+# Download tomcat software and extract it.
+sudo yum install wget unzip -y
+
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.63/bin/apache-tomcat-9.0.63.tar.gz
+sudo tar -xvf apache-tomcat-9.0.63.tar.gz
+sudo rm apache-tomcat-9.0.63.tar.gz
+sudo mv apache-tomcat-9.0.63 tomcat9
+sudo chmod 777 -R /opt/tomcat9
+sudo chown ec2-user -R /opt/tomcat9
+sh /opt/tomcat9/bin/startup.sh
+# create a soft link to start and stop tomcat
+sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+sudo yum update -y
+starttomcat
+
