6671250606b960866d262077b414a740320e3de3
yongtoh <yongtoh@gmail.com>
2022-06-26 11:29:24 +0000

    First Version

---
+++ sonarinstallation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author: Landmark Technology
+# CentOs or REHEL 7/8
+
+#As a good security practice, SonarQuber Server is not advised to run sonar service as a root user, 
+#so create a new user called sonar and grant sudo access to manage sonar services as follows
+# 1. create a sonar 
+sudo useradd sonar
+# 2. Grand sudo access to sonar user
+sudo echo "sonar ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/sonar
+sudo chown -R sonar:sonar /opt/sonarqube/
+sudo chmod -R 775 /opt/sonarqube/
+hostname sonar
+sudo su - sonar
+## 3. Enable PasswordAuthentication
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo service sshd restart
+
+# 4. Install JAVA pre-requisite
+cd /opt
+sudo yum -y install unzip wget git
+sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+sudo yum install jdk-8u131-linux-x64.rpm -y
+# 5. Download the SonarqQube Server software. 
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.8.zip
+sudo unzip sonarqube-7.8.zip
+sudo rm -rf sonarqube-7.8.zip
+sudo mv sonarqube-7.8 sonarqube
+
+# start sonarqube as sonar user using relative path
+sudo su - sonar  
+cd /opt/sonarqube/bin/linux-x86-64/ 
+sh sonar.sh start
+# or start sonarqube as sonar user using absolute path
+sh /opt/sonarqube/bin/linux-x86-64/sonar.sh start 
+sh /opt/sonarqube/bin/linux-x86-64/sonar.sh status
+#sh /opt/sonarqube/bin/linux-x86-64/sonar.sh start | stop | status | restart
